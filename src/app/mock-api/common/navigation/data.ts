e88b6f166c302c4f666e0f95e5076a71bad54d55
Janglee <votantai4899@gmail.com>
2023-10-08 13:12:10 -0700

    first commit

---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,67 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    }
+];
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    }
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    }
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id: 'dashboard',
+        title: 'Dashboard',
+        type: 'basic',
+        icon: 'heroicons_outline:chart-pie',
+        link: '/dashboards'
+    },
+    {
+        id: 'configurations',
+        title: 'Configurations',
+        type: 'basic',
+        icon: 'heroicons_outline:wrench',
+        link: '/configurations'
+    }
+];
