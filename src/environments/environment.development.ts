a197e464d88a36fb8f604286925cbbcf767af754
Janglee <votantai4899@gmail.com>
2023-11-16 01:24:29 +0700

    update be url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // baseUrl: 'https://localhost:7222',
-    baseUrl: 'https://f568-171-232-7-224.ngrok-free.app',
+    baseUrl: 'https://942d-171-232-7-224.ngrok-free.app',
     // baseUrl: 'https://plantdetectionservice.azurewebsites.net',
 };
